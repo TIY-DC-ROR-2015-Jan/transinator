1a1d8ac8af447a39baf70f9bb99567e37c455558
Alexander Blythe <Alexander.blythe@gmail.com>
2015-03-09 16:11:18 -0400

    Bike Stations Load

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -1,12 +1,15 @@
 class Bike < Station
   include HTTParty
   base_uri 'http://www.capitalbikeshare.com'
+  
   def self.get_bike_stations
     data = get('/data/stations/bikeStations.xml')
 
     data["stations"]["station"].each do |station|
       s = Station.find_or_create_by(terminalName: station["terminalName"].to_i)
-      s.update! name: s["name"], lat: s["lat"], long: s["long"], bikes: s["nbBikes"], s["nbEmptyDocks"]
+      s.update! type: "Bike", name: station["name"], lat: station["lat"], long: station["long"], bikes: station["nbBikes"], empty: station["nbEmptyDocks"]    
     end
+      
   end
+
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -2,4 +2,12 @@ class Station < ActiveRecord::Base
   has_many :user_stations
   has_many :users, through: :user_stations
 
+
+  def check_distance 
+    user_lat = current_user.lat.to_f
+    user_long = current_user.long.to_f
+    station_lat = self.lat.to_f
+    station_long = self.long.to_f
+    distance = Haversine.distance(user_lat, user_long, station_lat, station_long)
+  end
 end
