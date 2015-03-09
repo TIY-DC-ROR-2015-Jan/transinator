c102a27fe333f08f21addf7a95f3e7782e375c63
Alexander Blythe <Alexander.blythe@gmail.com>
2015-03-09 12:36:36 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
  root to: "home#index"
+
+ scope '/dashboard' do
+  get "/" => 'dashboard#show', as: 'dashboard'
+  get '/bikes' => 'bikes#index', as: 'bikes'
+  get '/buses' => 'buses#index', as: 'buses'
+  get '/rails' => 'rails#index', as: 'rails'
+  resources :station, only: [:create, :new, :destroy] 
+ end
+
+patch '/users/:id' => 'users#update', as: 'edit_user_location'
+
 end
