fb635ae91c8caab261f1f537f11446a0285912af
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-09 22:03:29 -0400

    added dotenv gem/support for loading WMATA apikey

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -2,10 +2,9 @@ class Bus < Station
   include HTTParty
 
   def self.get_user_bus_predictions(user)
-    wmatakey = File.read('.env')
     @bus_stop_info = []
     user.stations.where(type: "Bus").each do |stop| 
-      @bus_stop_info << HTTParty.get("https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=#{stop.api_id}&api_key=#{wmatakey}")
+      @bus_stop_info << HTTParty.get("https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=#{stop.api_id}&api_key=#{ENV["WMATA_APIKEY"]}")
     end
     @bus_stop_info
   end
--- app/views/buses/index.json.jbuilder
+++ app/views/buses/index.json.jbuilder
@@ -1,12 +1,13 @@
 @predictions = @bus_stop_info["Predictions"]
+binding.pry
 
+json.stop_name @bus_stop_info["StopName"]
 json.predictions @predictions do |p|
   json.route_id p["RouteID"]
   json.direction_text p["DirectionText"]
   json.minutes p["Minutes"]
 end
 
-json.stop_name @bus_stop_info["StopName"]
 
 
 
