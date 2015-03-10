dde8c650342608eccb14a2fd52eafa06c8c22ddc
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-09 21:43:02 -0400

    working refactor of buses with logic in model

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,14 +1,7 @@
 class BusesController < ApplicationController
 
-  include HTTParty
- 
-
   def index
-    wmatakey = File.read('.env')
-    @bus_stop_info = []
-    current_user.stations.where(type: "Bus").each do |stop| 
-      @bus_stop_info << HTTParty.get("https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=#{stop.api_id}&api_key=#{wmatakey}")
-    end
+    @bus_stop_info = Bus.get_user_bus_predictions(current_user)
     @bus_stop_info = @bus_stop_info[0]
   end
 
--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,4 +1,16 @@
 class Bus < Station
+  include HTTParty
+
+  def self.get_user_bus_predictions(user)
+    wmatakey = File.read('.env')
+    @bus_stop_info = []
+    user.stations.where(type: "Bus").each do |stop| 
+      @bus_stop_info << HTTParty.get("https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID=#{stop.api_id}&api_key=#{wmatakey}")
+    end
+    @bus_stop_info
+  end
+
+
 
 
 
