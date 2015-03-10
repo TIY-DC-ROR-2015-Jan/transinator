83490f1b800721fea5e87ac66e6f705dbc55ca85
Mallory Bucell <m.f.bucell@gmail.com>
2015-03-10 00:20:04 -0400

    refactor user rail predictions into smaller methods

--- app/models/rail.rb
+++ app/models/rail.rb
@@ -2,13 +2,15 @@ class Rail < Station
   include HTTParty
 
   def self.get_user_rail_predictions(user)
+    rail_info = HTTParty.get("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/#{Rail.find_user_rail_station_ids(user)}?api_key=#{ENV["WMATA_APIKEY"]}")
+    return rail_info
+  end
+
+  def self.find_user_rail_station_ids(user)
     station_ids = []
     stations = user.stations.where(type: "Rail")
     stations.each { |s| station_ids.push(s.api_id.to_s) }
     station_ids = station_ids * ","
-    rail_info = HTTParty.get("https://api.wmata.com/StationPrediction.svc/json/GetPrediction/#{station_ids}?api_key=#{ENV["WMATA_APIKEY"]}")
-    return rail_info
+    station_ids
   end
-
-  #def get_user_rail_ids?
 end
